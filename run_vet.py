d5dc90de8324d749c44d7b134295a50725e86a89
Jillian Guillen <JGuillen@spartaglobal.com>
2019-09-30 23:31:00 +0100

    4 out of 4 user stories complete

--- run_vet.py
+++ run_vet.py
@@ -12,20 +12,19 @@ vet1 = Veterinarian('Torn ligaments', 'Dr. Wendy', 'Williams', '07336153421')
 vet2 = Veterinarian('Flu jabs', 'Dr. Philip', 'Mitchel', '02085475475')
 vet3 = Veterinarian('General', 'Dr. Caitlyn', 'Shameer', '01895466422')
 
+# As a user I can add a pet to appointment
 appt1 = Appointment('Broken toe', '1 October 2019', '£85', vet1)
 appt2 = Appointment('Blocked nose', '7 October 2019', '£45', vet2)
 appt3 = Appointment('General check-up', '2 October 2019', '£30', vet3)
 
-# As a user I can add a pet to appointment
 booking1 = appt1.add_pet(pet1), vet1
 booking2 = appt2.add_pet(pet4), vet2
 booking3 = appt3.add_pet(pet3), appt3.add_pet(pet2), vet3
 
-
 # As a user I can list all appointments (need to see what pet's are there)
 for booking in (appt1, appt2, appt3):
-    print(booking.appointment_info())
-
+   print(booking.appointment_info())
 
 # As a user I can get pet details and owner details for a specific pet
-    # Create a method that lists owner details and pet details
+for pet in (pet1, pet2, pet3, pet4):
+    print(pet.pet_details())
